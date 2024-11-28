e15a245b76cf2b587f7fd73e6814fcebe166d47f
moustaouiSalaheddine <moustaoui.salaheddine@gmail.com>
2024-11-28 20:21:07 +0100

    Initial Commit

---
+++ Calculatrice App/Calculatrice_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculatrice_AppApp.swift
+//  Calculatrice App
+//
+//  Created by Mac  on 28/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Calculatrice_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculatrice AppTests/Calculatrice_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Calculatrice_AppTests.swift
+//  Calculatrice AppTests
+//
+//  Created by Mac  on 28/11/2024.
+//
+
+import Testing
+@testable import Calculatrice_App
+
+struct Calculatrice_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
